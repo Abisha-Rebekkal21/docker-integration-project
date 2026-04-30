cd053b2e37db23e6c4d925d82008b8eef1685f67
Abisha-Rebekkal21 <abisharebekkalt@gmail.com>
2026-04-30 09:22:54 +0530

    test

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -5,7 +5,7 @@ describe("Integration Tests", () => {
   it("GET / should return 200", async () => {
     const res = await request("http://app:3000").get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.body.message).toBe("Hello from Docker App 🚀");
+    expect(res.body.message).toBe("Hello from Docker App");
   });
 
   it("GET /api/test should return success true", async () => {
