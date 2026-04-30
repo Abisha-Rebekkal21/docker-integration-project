cba36ef4809b5fa8b81038ae563f892d3182f757
Abisha-Rebekkal21 <abisharebekkalt@gmail.com>
2026-04-30 09:17:41 +0530

    test

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from Docker App 🚀" });
+  res.status(200).json({ message: "Hello from Docker App" });
 });
 
 app.get("/api/test", (req, res) => {
